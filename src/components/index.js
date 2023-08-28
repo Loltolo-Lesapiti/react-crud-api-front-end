4e50c39326d66acd8e57a7d5a83e2eb91543fb17
petro-lesapiti <petrolesapiti@gmail.com>
2023-08-28 15:25:42 +0300

    implement routing.

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Create from "./create";
+import Main from "./main";
+import Navbar from "./navbar";
+import Show from "./show";
+import Footer from "./footer";
+import Nopage from "./Nopage";
+import EditTask from "./edit";
+
+export {Create, Main, Navbar, Show, Footer, Nopage, EditTask};

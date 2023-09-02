92158100bac215b3ec065af234049742e1bb5156
petro-lesapiti <petrolesapiti@gmail.com>
2023-09-02 12:51:56 +0300

    Axios files.

--- src/http_common.js
+++ src/http_common.js
@@ -1,8 +1,9 @@
-// Initializing axios 
-import axios from "axios";
-export default axios.create({
-    baseURL: "http://127.0.0.1:3000/api/v1/",
-    headers: {
-        "Content-Type": "application/json"
-    }
-})
+// // Initializing axios 
+// import axios from "axios";
+// export default axios.create({
+//     withCredentials: true,
+//     baseURL: "http://127.0.0.1:3000/api/v1/",
+//     headers: {
+//         "Content-Type": "application/json"
+//     }
+// })
--- src/services/task_service.js
+++ src/services/task_service.js
@@ -1,23 +1,23 @@
-import apiConfig from "../http_common";
-// Creating CRUD methods.
-const displayTasks=()=>{
-    return apiConfig.get('/tasks');
-}
+// import apiConfig from "../http_common";
+// // Creating CRUD methods.
+// const displayTasks=()=>{
+//     return apiConfig.get('/tasks');
+// }
 
-const show=(id)=>{
-    return apiConfig.get(`/tasks/${id}`);
-}
+// const show=(id)=>{
+//     return apiConfig.get(`/tasks/${id}`);
+// }
 
-const edit=(id, data)=>{
-    return apiConfig.put(`/tasks/${id}`, data);
-}
+// const edit=(id, data)=>{
+//     return apiConfig.put(`/tasks/${id}`, data);
+// }
 
-const destroy=(id)=>{
-    return apiConfig.delete(`/tasks/${id}`);
-}
+// const destroy=(id)=>{
+//     return apiConfig.delete(`/tasks/${id}`);
+// }
 
-const create=(id, data)=>{
-    return apiConfig.post(`/tasks/${id}`, data);
-}
+// const create=(id, data)=>{
+//     return apiConfig.post(`/tasks/${id}`, data);
+// }
 
-export default {displayTasks, create,destroy,edit};
+// export default {displayTasks, create,destroy,edit};
